356b1abde93337c5c236174a3b2827a44aa6b879
manueladriancaceres-ort <manuel.caceres@ort.edu.ar>
2022-03-17 08:10:45 -0300

    objetos

--- clase1/ejemplo/main.js
+++ clase1/ejemplo/main.js
@@ -1,19 +1,13 @@
 const arrays = require('./arrays/arrays.js'); 
-
 const miArreglo = [30,45,34,23];
-
 let resultado = arrays.calcularSuma(miArreglo); 
-
 console.log(resultado);
-
 console.log(arrays.sumaPares(miArreglo));
 
 // objetos
 
-const persona = {
-    "nombre" : "Juan",
-    "apellido" : "Gomez"
-}
+const utilesObjetos = require('./objetos/objetos.js'); 
+console.log(utilesObjetos.persona());
 
-console.log(persona);
+
 
---
+++ clase1/ejemplo/objetos/objetos.js
@@ -0,0 +1,8 @@
+exports.persona = function() {
+    const persona = {
+        "nombre" : "Juan",
+        "apellido" : "Gomez"
+    }
+    
+    return persona;
+}
