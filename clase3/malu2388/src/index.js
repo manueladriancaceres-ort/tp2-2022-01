4cbc67ed22d6df30659ff9dbafeb1d968b4b0954
malu2388 <malu2388@gmail.com>
2022-03-31 22:54:36 -0300

    proyecto con NPM

---
+++ clase3/malu2388/src/index.js
@@ -0,0 +1,16 @@
+console.log("Index de Malu Rodriguez");
+
+import { caja } from './service/caja.js'
+
+const cajaCopia = {...caja };
+
+cajaCopia.codigo = 3000;
+
+console.log(cajaCopia);
+console.log(caja);
+
+const { codigo, descripcion } = caja;
+
+console.log(codigo);
+
+console.log(descripcion);
