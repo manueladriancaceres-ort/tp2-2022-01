14838b5af114aca7c628b451e506246c5f0deef7
sabrinaValansi <sabrivalan@hotmail.com>
2022-04-07 22:54:55 -0300

    cambios

--- clase4/SabrinaValansi/src/services/Promesas.js
+++ clase4/SabrinaValansi/src/services/Promesas.js
@@ -1,6 +1,10 @@
 const miPromesa = new Promise((resolve,reject) => {
     setTimeout(()=> resolve("Exito en 2 segundos"),2000);
 });
-miPromesa.then(mensaje=>console.log(mensaje));
+//miPromesa.then(mensaje=>console.log(mensaje));
 
+async function unProceso(){
+    console.log(await miPromesa);
+}
+await unProceso();
 console.log("terminado");
