9442adf1d45f2e680e9fa15550a98a1a3dec8491
Martin Corizzo <macorizzo@gmail.com>
2022-04-08 01:36:23 -0300

    clase 4-2

--- clase4/macorizzo/src/modelos/caja.js
+++ clase4/macorizzo/src/modelos/caja.js
@@ -2,14 +2,25 @@ const caja = {
     codigo: 100,
     descripcion: 'Caja del dia tal..',
     // completar con mas datos
-    comprobantes: [{codigo:1,importe:1000,tipo:'I'},{codigo:1,importe:500,tipo:'E'}],
+    comprobantes: [{codigo:8,importe:1000,tipo:'I'},{codigo:2,importe:500,tipo:'E'},
+    {codigo:3,importe:1000,tipo:'I'},{codigo:4,importe:500,tipo:'E'} ],
     // completar con mas datos
     valores: {"mil":5,"cien":45,"diez":5},
     sumaComprobantes() {
-
+        return this.comprobantes.reduce((suma,comprobante) => (suma+comprobante.importe),0);
     },
     sumaValores() {
-
+        return this.valores.mil * 1000 + this.valores.cien * 100 + this.valores.diez * 10;
+    },
+    comprobantesMayores(importe) {
+        return this.comprobantes.filter(comprobante => comprobante.importe > importe );
+    },
+    ordenarComprobantesPorCodigo() {
+        this.comprobantes.sort((a,b) => a.codigo-b.codigo );
+    },
+    listaComprobantes() {
+        return this.comprobantes.map( 
+            comprobante => {return  { numero: comprobante.codigo, tipoComp: comprobante.tipo } }   );
     },
     estaCerradaCierreCaja() {
         // suma de comprobantes - valores == 0
