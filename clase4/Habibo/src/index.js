7c65fa72b2305b78e59d3167e681b2c698f94e8b
Matias Habibo <matihabibo@gmail.com>
2022-04-07 22:51:46 -0300

    habibo sync

---
+++ clase4/Habibo/src/index.js
@@ -0,0 +1,12 @@
+console.log('Index de mi proyecto');
+
+import {caja} from './modelos/caja.js';
+
+//console.log(caja);
+console.log(caja.sumaComprobantes());
+
+console.log(caja.comprobantesMayores(700));
+
+console.log(caja.ordenarPorCodigo());
+
+console.log(caja.listaDeComprobantes());
